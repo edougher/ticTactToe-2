e6b0c619d538cf3723686230a9e801b8d3a818e9
Erin Dougher <edougher@students.mchenry.edu>
2018-02-14 08:13:38 -0600

    Initial Commit

---
+++ ticTactToe-2/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  ticTactToe-2
+//
+//  Created by Aaron Dougher on 2/14/18.
+//  Copyright © 2018 Aaron Dougher. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
